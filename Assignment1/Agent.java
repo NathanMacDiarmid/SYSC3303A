f41e5f1e3447433f02ff3fcbc4b6f8e906af0d43
Nathan MacDiarmid <nathanmacdiarmid@gmail.com>
2023-01-18 21:30:11 -0500

    working on assignment 1

--- Assignment1/Agent.java
+++ Assignment1/Agent.java
@@ -12,4 +12,12 @@ public class Agent extends Thread {
     public synchronized void put() {
 
     }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+    }
 }
